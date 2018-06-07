9f8ccc1f5c7b6d05e874ca5e882df8850cdc2f7e
jirenguSanyue <562441461@qq.com>
2018-06-07 16:45:31 +0800

    邮箱格式及存在提示和两次密码一致与否

--- server.js
+++ server.js
@@ -18,56 +18,80 @@ var server = http.createServer(function(request, response){
   var method = request.method
 
   /******** 从这里开始看，上面不要看 **********/ 
-
-if(path === '/'){
-  var string = fs.readFileSync('./index.html','utf8')
+if(path === '/sign_up' && method === 'GET'){
+  let string = fs.readFileSync('./sign_up.html','utf-8')
   response.statusCode = 200
-  var amount = fs.readFileSync('./db','utf8')   //金额100
-  string = string.replace('&&&amount&&&',amount)
   response.setHeader('Content-Type','text/html;charset=utf-8')
   response.write(string)
   response.end()
-}else if(path === '/sign_up' && method === 'GET'){
-    let string = fs.readFileSync('./sign_up.html','utf-8')
-    response.statusCode = 200
-    response.setHeader('Content-Type','text/html;charset=utf-8')
-    response.write(string)
-    response.end()
-    //我新增了一个"登录注册页面"的路由
+  //我新增了一个"登录注册页面"的路由
 
 }else if(path === '/sign_up' && method === 'POST'){
-    
-    let body = []      //请求体
-    request.on('data', (chunk) => {
-      body.push(chunk);
-    }).on('end', () => {
-      body = Buffer.concat(body).toString()
-      // at this point, `body` has the entire request body stored in it as a string
-      console.log(body)
-      response.statusCode = 200
-      response.end()
+  readBody(request).then((body)=>{
+    let strings = body.split('&')  //['email=1','password=2','password_comfirm=3']
+    let hash = {}
+    strings.forEach((string)=>{
+      //string == 'email=1'
+      let parts = string.split('=')    //['email','1']
+      let key = parts[0]
+      let value = parts[1]
+      /*hash[key] = value  //hash['email']='1'  email里的@符号变成了%40故失效*/
+      hash[key] = decodeURIComponent(value)
     })
-}else if(path === '/style.css'){
-  var string = fs.readFileSync('./style.css','utf8')
-  response.statusCode = 200
-  response.setHeader('Content-Type','text/css')
-  response.write(string)
-  response.end()
-}else if(path === '/main.js'){
-  var string = fs.readFileSync('./main.js','utf8')
-  response.statusCode = 200
-  response.setHeader('Content-Type','application/javascript')
-  response.write(string)
-  response.end()
-}else if(path==='/pay'){
-  var amount = fs.readFileSync('./db','utf8')  //100
-  var newAmount = amount - 1
-  fs.writeFileSync('./db',newAmount)
-  response.setHeader('Content-Type','application/javascript')
-  response.statusCode = 200
-  response.write(`${query.callback}(undefined,'success')`)
-  response.end()
+    /*let email = hash['email']
+    let password = hash['password']
+    let password_comfirm = hash['password_comfirm']*/
+    let {email,password,password_comfirm} = hash
+    if(email.indexOf('@') === -1){
+      response.statusCode = 400
+      response.setHeader('Content-Type','application/json;charset=utf-8')
+      response.write(`
+      {
+        "errors":{
+          "email":"invalid"
+        },
+        "hint":{
+          "account":""
+        }
+      }`)
+    }else if(hash['password_confirm']!==hash['password']){
 
+      response.statusCode = 400
+      response.write("password not match")
+    }else {
+      var users = fs.readFileSync('./db/users','utf-8')
+      users = JSON.parse(users)   //[]
+      
+      let inUse = false
+      for(let i = 0;i < users.length;i ++){
+        let user = users[i]
+        if(user.email === email){
+          inUse = true
+          break
+        }
+      }
+      if(inUse){
+        response.statusCode = 400
+        response.write(`
+        { 
+          "errors":{
+            "email":""
+          },
+          "hint":{
+            "account":"exist"
+          }
+        }`)
+      }else{
+        users.push({email: email, password: password})
+        //console.log(users)
+        var usersString = JSON.stringify(users)
+        //console.log(usersString)
+        fs.writeFileSync('./db/users',usersString)
+        response.statusCode = 200
+      }
+    }
+    response.end()
+  })
 }else{
   response.stateCode = 404
   response.setHeader('Content-Type','text/html;charset=utf-8')
@@ -79,6 +103,20 @@ if(path === '/'){
 
   /******** 代码结束，下面不要看 ************/
 })
+
+function readBody(request){
+  return new Promise((resolve,reject)=>{
+    let body = []      //请求体
+    request.on('data', (chunk) => {
+      body.push(chunk);
+    }).on('end', () => {
+      body = Buffer.concat(body).toString()
+      // at this point, `body` has the entire request body stored in it as a string
+      //console.log(body)
+      resolve(body)
+    })
+  })
+}
 
 server.listen(port)
 console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
