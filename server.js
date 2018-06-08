5195dc679c7bb136cb2f3cca72b0f1e612f6deb1
jirenguSanyue <562441461@qq.com>
2018-06-08 09:05:50 +0800

    Cookie试验

--- server.js
+++ server.js
@@ -18,7 +18,46 @@ var server = http.createServer(function(request, response){
   var method = request.method
 
   /******** 从这里开始看，上面不要看 **********/ 
-if(path === '/sign_up' && method === 'GET'){
+if(path === '/'){
+  let string = fs.readFileSync('./class.html','utf-8')
+  /*let users = fs.readFileSync('./db/users','utf-8')
+  try{
+    users = JSON.parse(users)  //[]
+  }catch(exception){
+    users = []
+  }
+  console.log('request.headers.cookie')
+  console.log(request.headers.cookie)*/
+  let cookies = request.headers.cookie.split(';')  //['xsj@qq.com','boe.qq.com','562441461@qq.com']
+  let hash = {}
+  for(let i = 0;i < cookies.length;i ++){
+    let parts = cookies[i].split('=')
+    let key = parts[0]
+    let value = parts[1]
+    hash[key] = value
+  }
+  //console.log('hash')
+  //console.log(hash)
+  let email = hash.sign_in_email
+  let users = fs.readFileSync('./db/users','utf-8')
+  users = JSON.parse(users)
+  let foundUser
+  for(let i=0;i<users.length;i++){
+    if(users[i].email === email){
+      foundUser = users[i]
+      break
+    }
+  }
+  if(foundUser){
+    string=string.replace('__password__',foundUser.password)
+  }else{
+    string=string.replace('__password__','未知')
+  }
+  response.statusCode = 200
+  response.setHeader('Content-Type','text/html;charset=utf-8')
+  response.write(string)
+  response.end()
+}else if(path === '/sign_up' && method === 'GET'){
   let string = fs.readFileSync('./sign_up.html','utf-8')
   response.statusCode = 200
   response.setHeader('Content-Type','text/html;charset=utf-8')
@@ -90,6 +129,53 @@ if(path === '/sign_up' && method === 'GET'){
         response.statusCode = 200
       }
     }
+    response.end()
+  })
+}else if(path === '/sign_in' && method === 'GET'){           //新增登录请求页面的路由
+  let string = fs.readFileSync('./sign_in.html','utf-8')
+  response.stateCode = 200
+  response.setHeader('Content-Type','text/html;charset=utf-8')
+  response.write(string)
+  response.end()
+}else if(path==='/sign_in' && method==='POST'){      //登录页面提交表单
+  readBody(request).then((body)=>{
+    let strings = body.split('&')  //['email=1','password=2','password_comfirm=3']
+    let hash = {}
+    strings.forEach((string)=>{
+      //string == 'email=1'
+      let parts = string.split('=')    //['email','1']
+      let key = parts[0]
+      let value = parts[1]
+      /*hash[key] = value  //hash['email']='1'  email里的@符号变成了%40故失效*/
+      hash[key] = decodeURIComponent(value)
+    })
+    let {email,password} = hash
+    console.log('email')
+    console.log(email)
+    console.log('password')
+    console.log(password)
+
+    var users = fs.readFileSync('./db/users','utf-8')
+    try{
+      users = JSON.parse(users)    //[]
+    }catch(exception){
+      users = []
+    }
+    
+    let found
+    for(let i = 0;i < users.length;i ++){
+      if(users[i].email === email && users[i].password === password){
+        found = true
+        break
+      }
+    }
+    if(found){
+      response.setHeader('Set-Cookie',`sign_in_email=${email}`)
+      response.statusCode = 200
+    }else{
+      response.statusCode = 401   //Unauthorized
+    }
+
     response.end()
   })
 }else{
